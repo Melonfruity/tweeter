8b4a9e153e1a4b321ac4f523e6be882276b49f9f
melonfruity <eric.luo24@gmail.com>
2019-12-18 21:06:31 -0500

    added render tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,58 @@
  */
 
 // gets the days ago from the time the tweets are retrieved from the api
+const getDaysAgo = (time) => {
+  let secondsAgo = new Date() - time;
+  return `${Math.floor(secondsAgo / 86400000)} days ago`;
+}
+
+const createTweetElement = (tweet) => {
+  let $tweet = $('<article>').addClass('tweet');
+  let $header = $('<div>').addClass('tweet-header');
+  let $content = $('<div>').addClass('tweet-content');
+  let $footer = $('<div>').addClass('tweet-footer');
+
+  // header
+  let $userInfo = $('<div>').addClass('tweet-user')
+  let $profilePicture = $('<img>').attr('src', `${tweet.user.avatars}`).addClass('pic')
+  let $name = $('<div>').text(`${tweet.user.name}`).addClass('name');
+  let $handle = $('<div>').text(`${tweet.user.handle}`).addClass('handle')
+  
+  // append header elements
+  $userInfo
+    .append($profilePicture)
+    .append($name);
+  $header
+    .append($userInfo)
+    .append($handle)
+
+  // content
+  $content.text(`${tweet.content.text}`);
+
+  // const test = `<script>alert('hi')</script>`  
+  // $content.html(`${test}`); can run malicious scripts
+  // Safe HTML
+  /*
+    const escape =  function(str) {
+      let div = document.createElement('div');
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    }
+    const safeHTML = `<p>${escape(textFromUser)}</p>`;
+  */
+  
+  // footer 
+
+
+// makes a get request to the api to retrieve the tweets
+const loadTweets = (container) => {
+  // return tweets
+  $.ajax('http://localhost:8080/tweets/', { method: 'GET' })
+    .then(function (tweets) {
+      renderTweets(container, tweets);
+  });;
+}
+
 // Jquery ready
 (function($, window, document) {
 
@@ -12,6 +64,18 @@
 
   // Listen for the jQuery ready event on the document
   $(function() {
+
+    const $errorSection = $('#error-section');
+    const $tweetsContainer = $('#tweets-container');
+    const $error = $('#error-message');
+
+    // Navbar slide listener
+    $('#new-tweet-toggle').click(function(){
+      $("#new-tweet").slideToggle("slow");
+    });
+
+    // Loads in and renders the initial tweets
+    loadTweets($tweetsContainer)
 
     // submit posts to the api
     $("#tweet-form").submit(function( event ) {
