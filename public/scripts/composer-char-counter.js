26c05a513da4f7345034ce26ca6e5e3efb9d05a5
melonfruity <eric.luo24@gmail.com>
2019-12-19 15:44:28 -0500

    finished linting composer-char-counter.hs

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,24 +1,27 @@
 
-(function($, window, document) {
+( function( $, _window, _document ) {
 
-  // The $ is now locally scoped 
+  // The $ is now locally scoped
 
   // Listen for the jQuery ready event on the document
-  $(function() {
+  $( function() {
 
-    $('textarea').on("input", function(){
+    $( "textarea" ).on( "input", function() {
       let maxlength = 140;
-      let currentLength = $(this).val().length;
-      const counter = $(this).siblings("#submit-tweet").find(".counter")[0];
+      let currentLength = $( this ).val().length;
+      const counter = $( this ).siblings( "#submit-tweet" ).find( ".counter" )[ 0 ];
       let count = maxlength - currentLength;
-      count < 0 ? $(counter).css("color", "red") : $(counter).css("color", "black");
       
-      if (count <= maxlength) {
-        $(counter).text(`${count}`);
+      // sets text black or red
+      $( counter ).css( "color", ( count < 0 ) ? "red" : "black" );
+      
+      if ( count <= maxlength ) {
+        $( counter ).text( `${count}` );
       }
-    });
+    } );
 
-  });
+  } );
 
-}(window.jQuery, window, document));
+} )( window.jQuery, window, document );
+
 // The global jQuery object is passed as a parameter
